3d05fc6a1944263c2044fde75fac65bad61e1c5e
Thomas <84379061+KomaedaN@users.noreply.github.com>
2022-12-22 19:28:46 +0100

    Update book(competition, club), now you can't book a place on a post dated competition

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template, request, redirect, flash, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
@@ -33,7 +34,11 @@ def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html', club=foundClub, competition=foundCompetition)
+        if datetime.strptime(foundCompetition['date'], '%Y-%m-%d %H:%M:%S') < datetime.now():
+            flash("This competition is over you can't purchase places")
+            return render_template('welcome.html', club=foundClub, competitions=competitions)
+        else:
+            return render_template('booking.html', club=foundClub, competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
@@ -52,6 +57,7 @@ def purchasePlaces():
         flash("You can't purchase more then 12 places")
 
     elif placesRequired <= points_available:
+        club['points'] = points_available - placesRequired
         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
         flash('Great-booking complete!')
 
@@ -59,6 +65,7 @@ def purchasePlaces():
         flash('You need more points')
 
     return render_template('welcome.html', club=club, competitions=competitions)
+
 
 # TODO: Add route for points display
 
