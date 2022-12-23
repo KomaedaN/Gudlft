947bf4fe783cbf2a5bb770a03af8a97a6e4177ee
Thomas <84379061+KomaedaN@users.noreply.github.com>
2022-12-23 15:07:10 +0100

    Update book(competition, club), now you can't book a place on a post dated competition

--- server.py
+++ server.py
@@ -57,7 +57,6 @@ def purchasePlaces():
         flash("You can't purchase more then 12 places")
 
     elif placesRequired <= points_available:
-        club['points'] = points_available - placesRequired
         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
         flash('Great-booking complete!')
 
